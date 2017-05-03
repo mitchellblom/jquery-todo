da98d2de4475546ae3be2b3933825efa50c97eba
Mitchell Blom <mitchellblom@gmail.com>
2017-05-02 21:13:50 -0500

    user login switches view

--- javascripts/main.js
+++ javascripts/main.js
@@ -100,14 +100,34 @@ $(function() {
         	console.log("addUser", response);
         }).catch((error) => {
         	console.log("error in addUser", response);
-        })
+        });
         }).catch((error) => {
         	console.log("error in registerUser", error);
         });
     });
 
+    let clearLogin = () => {
+    	let email = $("").val();
+        let password = $("").val();
+        let username = $("").val();
+    };
 
+	$("#loginButton").click(() => {
+		let email  = $('#inputEmail').val();
+		let password = $("#inputPassword").val();
 
+		let user = {email, password};
+
+		FbApi.loginUser(user).then((response) => {
+			clearLogin();
+			$('#login-container').addClass('hide');
+			$('.main-container').removeClass('hide');
+			FbApi.writeDom(apiKeys);
+		}).catch((error) => {
+			console.log("error in loginUser: ", error);
+		});
+
+	});
 
 
 
